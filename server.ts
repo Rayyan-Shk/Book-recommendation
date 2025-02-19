f87783f2da9a9b3497b2564373a344e8128a8a4e
Rayyan-Shk <rayyanshaikhassociates@gmail.com>
2025-02-19 11:56:08 +0530

    Updated: Data on DB

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ mongoose
   .catch((err) => console.error(err));
 
 if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
